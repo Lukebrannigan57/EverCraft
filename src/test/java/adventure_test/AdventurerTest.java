0c4bd9002b9800ae11f97faf27dbc0201bca7049
Lukebrannigan57 <lukebrannigan57@gmail.com>
2020-02-01 16:29:48 -0500

    passing test canGetAlignment

--- src/test/java/adventure_test/AdventurerTest.java
+++ src/test/java/adventure_test/AdventurerTest.java
@@ -10,15 +10,22 @@ public class AdventurerTest {
 
     @Test
     public void canGetName() {
-        underTest = new Adventurer("Olaf");
+        underTest = new Adventurer("Olaf", "Evil");
         String expected = underTest.getName();
         assertEquals(expected, "Olaf");
     }
 
     @Test
     public void canSetName() {
-        underTest = new Adventurer("Abigail");
+        underTest = new Adventurer("Abigail", "Evil");
         String expected = underTest.getName();
         assertEquals(expected, "Abigail");
+    }
+
+    @Test
+    public void canSetAlignment() {
+        underTest = new Adventurer("Moriarty", "Evil");
+        String expected = underTest.getAlignment();
+        assertEquals(expected, "Evil");
     }
 }
