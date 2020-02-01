53e7763a5b32ec6daf8dc21030173e94e7b3924e
Lukebrannigan57 <lukebrannigan57@gmail.com>
2020-02-01 17:02:29 -0500

    passing tests can get hp and ac

--- src/test/java/adventure_test/AdventurerTest.java
+++ src/test/java/adventure_test/AdventurerTest.java
@@ -10,29 +10,43 @@ public class AdventurerTest {
 
     @Test
     public void canGetName() {
-        underTest = new Adventurer("Olaf", "Evil");
+        underTest = new Adventurer("Olaf", Adventurer.alignment.EVIL);
         String expected = underTest.getName();
         assertEquals(expected, "Olaf");
     }
 
     @Test
     public void canSetName() {
-        underTest = new Adventurer("Abigail", "Evil");
+        underTest = new Adventurer("Abigail", Adventurer.alignment.EVIL);
         String expected = underTest.getName();
         assertEquals(expected, "Abigail");
     }
 
     @Test
     public void canGetAlignment() {
-        underTest = new Adventurer("Moriarty", "Evil");
-        String expected = underTest.getAlignment();
-        assertEquals(expected, "Evil");
+        underTest = new Adventurer("Moriarty", Adventurer.alignment.EVIL);
+        Enum expected = underTest.getAlignment();
+        assertEquals(expected, Adventurer.alignment.EVIL);
     }
 
     @Test
     public void canSetAlignment() {
-        underTest = new Adventurer("Karl Marx", "Good");
-        String expected = underTest.getAlignment();
-        assertEquals(expected, "Good");
+        underTest = new Adventurer("Karl Marx", Adventurer.alignment.GOOD);
+        Enum expected = underTest.getAlignment();
+        assertEquals(expected, Adventurer.alignment.GOOD);
+    }
+
+    @Test
+    public void canGetArmorClass() {
+        underTest = new Adventurer("Joan of Arc", Adventurer.alignment.GOOD);
+        int expected = underTest.getArmorClass();
+        assertEquals(expected, 10);
+    }
+
+    @Test
+    public void canGetHitPoints() {
+        underTest = new Adventurer("John Brown", Adventurer.alignment.GOOD);
+        int expected = underTest.getHitPoints();
+        assertEquals(expected, 5);
     }
 }
